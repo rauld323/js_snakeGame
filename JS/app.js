d356395b4ccfbaab21c6f53514ee4367b0796a80
Raul Davila <rauldavila323@gmail.com>
2020-05-08 11:51:14 -0500

    Finished the layout but need to add audio files

--- JS/app.js
+++ JS/app.js
@@ -2,44 +2,114 @@ const cvs = document.getElementById('snakeCanvas');
 
 const ctx = cvs.getContext('2d'); // This will allow to draw on the canvas
 
-let box = 18;
+const box = 32;
 
 //Loading Images and Audio Files
-const lemon = new Image();
-lemon.src = "img/lemon.png"; //Will show background image;
+const ground = new Image();
+ground.src = "img/ground.png";
 
+const foodImg = new Image();
+foodImg.src = "img/food.png"; //Will show background image;
+
+/*
 let audioName= new Audio();
 audioName.src = "";
-audioName.play();
-
-const ground = new Image();
-ground.src = "https://i.ytimg.com/vi/xKYX2r0-mkA/hqdefault.jpg";
+//audioName.play();
+*/
 
 let snake =[];
-snake[0]= {x: 10* box, y:9* box};
-snake[1] = { x: 8 * box, y:10* box };
+
+snake[0]= {
+     x: 9* box, 
+     y: 10* box
+}; //position of snake
 
 let food = {
-   x:Math.floor(Math.random()* 17 +1)*box,
-   y:Math.floor(Math.random()* 15 +3)*box };
+   x : Math.floor(Math.random()* 17 +1)*box, // the position of the food will be between 1 box and 17 boxes
+   y : Math.floor(Math.random()* 15 +3)*box 
+};
 
 let score = 0;
 
+let d;
+
+document.addEventListener("keydown",direction);
+
+function direction(event) {
+    let key = event.keyCode;
+    if (key == 37 && d != "RIGHT") {
+        d = "LEFT";
+        left.play();
+    } else if (key == 38 && d != "DOWN") {
+        d = "UP";
+        up.play();
+    } else if (key == 39 && d != "LEFT") {
+        d = "RIGHT";
+        right.play();
+    } else if (key == 40 && d != "UP") {
+        d = "DOWN";
+        down.play();
+    }
+}
+
+
+//Snake Touches
+function collision(newHead,snake){
+    for(let i = 0; i < snake.length;i++){
+        if(newHead.x == snake[i].x && newHead.y == snake[i].y){
+            return true;
+            }
+    }
+    return false
+}
+
+
 function draw(){
-    ctx.drawImage(lemon, 40, 50, 25, 25);
-    ctx.drawImage(ground,0,0)
+    ctx.drawImage(ground,0,0) // 0,0 count for X and Y positions
     for(let i = 0; i < snake.length; i++){
-        ctx.fillStyle=(i == 0)? "blue":"green";
+        ctx.fillStyle = ( i == 0)? "green": "white";
         ctx.fillRect(snake[i].x, snake[i].y, box,box);
         
         ctx.strokeStyle="red";
-        ctx.strokeRect(snake[i].x, snake[i].y,box,box)
+        ctx.strokeRect(snake[i].x, snake[i].y,box,box);
     }
 
+    ctx.drawImage(foodImg, food.x, food.y);
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (d == "LEFT") snakeX -= box;
+    if (d == "UP") snakeY -= box;
+    if (d == "RIGHT") snakeX += box;
+    if (d == "DOWN") snakeY += box;
+
+    if (snakeX == food.x && snakeY == food.y) {
+        score++;
+        //eat.play();
+        food = { x: Math.floor(Math.random() * 17 + 1) * box, y: Math.floor(Math.random() * 15 + 3) * box}
+    } else {
+        snake.pop();
+    }
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    //game
+    if(snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 17 * box || collision(newHead,snake)) {
+            clearInterval(game);
+            dead.play();
+        }
+
+    snake.unshift(newHead);
+
+    ctx.fillStyle = "white";//The Score 
+    ctx.font = "45px Changa one";
+    ctx.fillText(score, 2 * box, 1.6 * box)
 
 }
-let game = setInterval(draw,100);
+let game = setInterval(draw,100); // draw is what is shown on canvas and 100 is how fast in miliseconds
 
-//Draw Snake
-ctx.fillStyle= "red";
-ctx.fillRect(100, 300,30,30);
+
